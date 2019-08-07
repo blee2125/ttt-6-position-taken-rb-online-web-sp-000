7ddee5445420eaccd34669ea7dae1007a968e775
favorable-device-5158 <blee2125@gmail.com>
2019-08-07 19:13:13 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,9 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] == "X" || "O"
-    return true
-  elsif board[index] == nil || "" || " "
-    return false
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    taken = false
+  else
+    taken = true
 
   end
 end
